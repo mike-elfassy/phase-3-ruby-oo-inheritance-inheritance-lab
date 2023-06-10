0604aaa97e8a98a67dbfb695a14d0ae832ff2902
Michael Elfassy <mike.elfassy@gmail.com>
2023-06-09 20:54:44 -0400

    commit tree ddc3b47cf55bb42620be661645aed08d31f4055c

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative './user'
+
+class Student < User
+    attr_accessor :knowledge
+    
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(topic)
+        self.knowledge << topic
+    end
 
 end
